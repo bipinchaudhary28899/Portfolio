644108185b78579e41932cb328a46952025c2f85
bipin chaudhary <bkumar28899@yahoo.com>
2025-11-01 10:47:45 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // enable static export for GitHub Pages
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  images: { unoptimized: true }, // disables server-side image optimization
+  output: 'export',
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
